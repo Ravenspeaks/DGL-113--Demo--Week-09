d76f481a455a951b42e5a946491dcb26906e85b5
Ravenspeaks <90291456+Ravenspeaks@users.noreply.github.com>
2023-04-12 11:21:21 -0700

    Update DOMops.js

--- docs/DOMops.js
+++ docs/DOMops.js
@@ -1,6 +1,6 @@
 (() => {
   // add a new item to the default (end) of the list
-  function addatDefault () {
+  function addatDefault() {
     const node = document.createElement('li');
     node.innerHTML = prompt('Enter a new item');
     const parent = document.querySelector('#section1 ol');
@@ -9,7 +9,7 @@
   }
 
   // add a new item to the given position of the list
-  function addatPosition () {
+  function addatPosition() {
     const node = document.createElement('li');
     node.innerHTML = prompt('Enter a new item');
     const count = document.querySelectorAll('#section1 li').length;
@@ -20,7 +20,7 @@
   }
 
   // Remove the item where the event is occurred
-  function removeItem (e) {
+  function removeItem(e) {
     const parent = e.target.parentElement;
     parent.removeChild(e.target);
     countListItems();
@@ -29,7 +29,7 @@
   /** add a new item to the end of the list. Afterward, evt handlers attached
    * to elements in #section1 will be disabled.
    */
-  function addinnerHTML () {
+  function addinnerHTML() {
     const htmlStr =
       '<p>Added using innerHTML of section 1. This will affect event handlers on the elements in section 1.</p>';
     const parent = document.querySelector('#section1');
@@ -37,7 +37,7 @@
     countListItems();
   }
 
-  function mainWindowLoaded (e) {
+  function mainWindowLoaded(e) {
     const btnAddDefault = document.querySelectorAll('button')[0];
     const btnAddAnyPos = document.querySelectorAll('button')[1];
     const btnAddInnerHTML = document.querySelectorAll('button')[2];
@@ -50,7 +50,7 @@
   }
 
   // this function finds and shows the number of items in the to-do-list
-  function countListItems () {
+  function countListItems() {
     const listItems = document.querySelectorAll('#section1 li');
     // alert("The ordered list contains " + listItems.length + " items");
     const span = document.querySelector('#section1 p span');
